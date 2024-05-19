43da99a9f3b62a405cec1ceab0799e9c80b0154d
Jordan Owens <jordanowens1125@gmail.com>
2024-05-19 11:15:33 -0500

    yoku yoga left sbs padding

--- yoku-yoga/src/app/home1/section4.jsx
+++ yoku-yoga/src/app/home1/section4.jsx
@@ -6,19 +6,21 @@ export default function HomeSection4() {
   const text =
     "Modern postural yoga consists largely but not exclusively of the practice of asanas. There were very few standing asanas before 1900. By 2012, there were at least 19 widespread styles from Ashtanga Yoga to Viniyoga. These emphasise different aspects including aerobic exercise, precision in the asanas, and spirituality in the Haṭha yoga tradition. For example, Bikram Yoga has an aerobic exercise style with rooms heated to 105 °F";
   return (
-    <FloatingLeftImgSBS
-      leftimg={"home1/section4.png"}
-      right={
-        <Content2
-          subheader={"our foundation is"}
-          header={"Modern Yoga"}
-          text={text}
-          buttons={[
-            { text: "Learn More", class: "btn-4", link: "/about1" },
-            { text: "Our Story", class: "btn-5", link: "/about2" },
-          ]}
-        />
-      }
-    />
+    <section id="foundation">
+      <FloatingLeftImgSBS
+        leftimg={"home1/section4.png"}
+        right={
+          <Content2
+            subheader={"our foundation is"}
+            header={"Modern Yoga"}
+            text={text}
+            buttons={[
+              { text: "Learn More", class: "btn-4", link: "/about1" },
+              { text: "Our Story", class: "btn-5", link: "/about2" },
+            ]}
+          />
+        }
+      />
+    </section>
   );
 }
