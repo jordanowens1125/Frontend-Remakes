ef7dadcecb89c984293c1f1e02da90104f32d75e
Jordan Owens <jordanowens1125@gmail.com>
2024-11-15 15:06:02 -0600

    icons not showing up in roofixer

---
+++ roofixer-envato/src/app/icons-test/up.jsx
@@ -0,0 +1,10 @@
+const UpSvgComponent = ({ fill = "currentColor", stroke = "currentColor" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill={fill}
+  >
+    <path d="M11.293 1.293a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1-1.414 1.414L13 4.414V22a1 1 0 0 1-2 0V4.414L7.707 7.707a1 1 0 0 1-1.414-1.414Z" />
+  </svg>
+);
+export default UpSvgComponent;
